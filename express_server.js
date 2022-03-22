21d8a28129d216bb119364b575acc1dd1586be36
Michael Law <mikelaw86@hotmail.com>
2022-03-22 10:20:46 -0400

    Add post route for deleting URLs

---
+++ express_server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const PORT = 8079;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+  "3b2kPr" : "http://www.bluejays.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  const longURL = urlDatabase[shortURL]
+  const templateVars = { shortURL: shortURL, longURL: longURL };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL
+  res.redirect(`/urls/${shortURL}`);       
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  const longURL = urlDatabase[shortURL]
+  res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL
+  delete (urlDatabase[shortURL])
+  res.redirect(`/urls`);       
+});
+
+function generateRandomString() {
+  return Math.random().toString(20).substring(0, 5);
+}
